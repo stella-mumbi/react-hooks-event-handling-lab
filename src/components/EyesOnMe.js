8c8cadabe285e10778d9ae4578fec6e7078cb163
stellamumbi <stellamumbi736@gmail.com>
2024-02-03 11:11:33 +0300

    commpleted lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+
+   let watching = () => console.log(`Good!`)
+   let distracted = () => console.log(`Hey! Eyes on me!`) 
+
+   return (
+      <React.Fragment>
+         <button onFocus={watching} onBlur={distracted}>Eyes on me</button>
+      </React.Fragment>
+   )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,18 @@
 // Code Keypad Component Here
 
+
+
+
 function Keypad (){
+    function handleChange(event){
+        console.log("Entering password...")
+
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password"placeholder="Entering password..."onChange={handleChange}/>
+        </div>
     )
 }
 
